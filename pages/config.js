0dc412fdc811781b014b3e28721ea829bc5204e1
SunilCz <86451565+SunilCz@users.noreply.github.com>
2023-06-02 04:50:00 +0545

    Update config.js

--- pages/config.js
+++ pages/config.js
@@ -2,3 +2,12 @@ export const ESEWA_TEST_PID = "Esewa_Task_4";
 export const ESEWA_SCD = "EPAYTEST";
 export const ESEWA_URL = "https://uat.esewa.com.np/epay";
 export const ESEWA_FRAUD_TEST_URL = "https://uat.esewa.com.np/epay/transrec";
+
+const config = {
+  ESEWA_TEST_PID,
+  ESEWA_SCD,
+  ESEWA_URL,
+  ESEWA_FRAUD_TEST_URL,
+};
+
+export default config;
